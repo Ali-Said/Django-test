3a73df36c6e6d0c30eb09c9bf242b586891161ba
Ali-Said <alimoussasaid@gmail.com>
2013-07-02 09:28:11 +0200

    little styling won't hurt

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -66,12 +66,11 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-# Additional locations of static files
+
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    'C:/Users/Ali Moussa/Documents/Mine Worken/sho3\'lana/blog project/Django-test/blogsite/blogsite/static' ,
 )
+# Additional locations of static files
 
 # List of finder classes that know how to find static files in
 # various locations.
